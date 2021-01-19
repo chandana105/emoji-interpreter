fefc3a7553f9ea08f217ca56d913980a33f59e21
chandana105 <39641650+chandana105@users.noreply.github.com>
2021-01-19 15:36:41 +0530

    checking the output to be in react view

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./styles.css";
 
 
 let likeCounter = 0;
+// we cant make this value to be in view
+let l = 0;
 
 export default function App() {
 
   const likeClickHandler = () => {
     likeCounter++; //processing
     console.log("liked " , likeCounter); //output
-
+    l = likeCounter
   }
+
   return (
     <div className="App">
       <h1>inside out</h1>
       {/* click on btn is input(interacting with react) */}
-      <button onClick={likeClickHandler}>Like Me</button>
+      <button onClick={likeClickHandler}>Like Me </button> {l} 
     </div>
   );
 }
