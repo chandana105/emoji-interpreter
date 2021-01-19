3ed32c71fc709a68a11c883223c1a27b5a2c854f
chandana105 <39641650+chandana105@users.noreply.github.com>
2021-01-19 16:01:00 +0530

    updated counter in view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 
-let likeCounter = 0;
-// we cant make this value to be in view
-let l = 0;
-
 export default function App() {
 
+
+const [likeCounter, setLikeCounter] = useState(0);
+
   const likeClickHandler = () => {
-    likeCounter++; //processing
-    console.log("liked " , likeCounter); //output
-    l = likeCounter
-  }
+    let newLikeCounterValue = likeCounter + 1; //processing
+    setLikeCounter(newLikeCounterValue) //processing/output
+    // passing the new value as arg to setlikecounter
+  };
+
+  // now it is in view ??? 
+  // we can use it in return
+  console.log('liked', likeCounter)
 
   return (
     <div className="App">
       <h1>inside out</h1>
       {/* click on btn is input(interacting with react) */}
-      <button onClick={likeClickHandler}>Like Me </button> {l} 
+      <button onClick={likeClickHandler}>Like Me </button> {likeCounter}
     </div>
   );
 }
+
+// wanted counter value to be in view so saving it as state value
